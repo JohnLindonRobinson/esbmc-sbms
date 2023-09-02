b82f710dae35ba15dba460bdd51506fe14059511
AadiNir <96436873+AadiNir@users.noreply.github.com>
2023-09-02 10:45:05 +0530

    Update use_of_deprecated_solidity_functions.sol

--- src/SWC-111/use_of_deprecated_solidity_functions.sol
+++ src/SWC-111/use_of_deprecated_solidity_functions.sol
@@ -1,36 +1,43 @@
-//SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract UseOfDeprecatedSolidityFunctions {
     function suicideContract() public {
         // Use selfdesturct(address) instead
-        suicide(msg.sender);
+        address payable addr = payable(address(msg.sender));
+
+        selfdestruct(addr);
     }
 
     function callcode() public {
         // Use call() instead
-        msg.sender.callcode();
+        msg.sender.call("");
+
     }
 
     function getBlockHash(uint x) public returns(bytes32) {
-        return block.blockhash(x);
+        return blockhash(x);
     }
 
-    function hash(uint x) public returns(bytes32) {
-        return sha3(x);
+    function hash(uint256 x) public returns(bytes32) {
+        bytes32 b = bytes32(uint256(x));
+        bytes memory d3 = abi.encodePacked(b);
+
+
+        return keccak256(d3);
     }
 
     function getGas() public returns(uint){
-        return msg.gas;
+        return gasleft();
     }
 
     function useThrow() public {
-        throw;
+        revert("an error occured");
     }
 
     function set_variables() public {
-        constant c = 1;
-        var v = 2;
+        uint c = 1;
+        uint v = 2;
     }
 
 }
